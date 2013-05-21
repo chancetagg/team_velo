1db0c3d9ec7848d95a9a53ce7bc40ca28e7c3186
Chance Tagg <chancetagg@gmail.com>
2013-05-21 13:23:43 -0700

    fixed sticky nav

--- Velo Gioielli/scripts/scripts.js
+++ Velo Gioielli/scripts/scripts.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function(){
+
 
 
 
@@ -32,67 +32,37 @@ function slideSwitch() {
 
 */
 
-alert('js working')
 
 
+$(document).ready(function() {
 
-
-.ready(function() {
- 
-        //Calculate the height of <header>
-        //Use outerHeight() instead of height() if have padding
-        var aboveHeight = $('header').outerHeight();
- 
-	//when scroll
-        $(window).scroll(function(){
- 
-	        //if scrolled down more than the header’s height
-                if ($(window).scrollTop() > aboveHeight){
- 
-	        // if yes, add “fixed” class to the <nav>
-	        // add padding top to the #content 
-	        
-	        
-	       
-                (value is same as the height of the nav)
-                $('navWrapper').addClass('fixed').css('left','50%').css('margin-left','-480px')
-                /*.next()
-                .css('padding-top','90px');*/
- 
-                } else {
- 
-	        // when scroll up or less than aboveHeight,
-                remove the “fixed” class, and the padding-top
-                $('navWrapper').removeClass('fixed')
-                
-                
-                /*.next()
-                .css('padding-top','0');
-                }*/
-        });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//alert('js working')
+	
+	//Calculate the height of <header>
+	//Use outerHeight() instead of height() if have padding
+    var aboveHeight = $('header').outerHeight();
+	
+	// when scroll
+    $(window).scroll(function(){
+		
+		//if scrolled down more than the header's height
+        if ($(window).scrollTop() > aboveHeight){
+			
+			// if yes, add "fixed" class to the <nav>
+			// add padding top to the #content (value is same as the height of the nav)
+            $('.navBar').addClass('fixed').css('top','0').next().css('padding-top','90px');
+        } else {
+			
+			// when scroll up or less than aboveHeight, remove the "fixed" class, and the padding-top
+            $('.navBar').removeClass('fixed').next().css('padding-top','0');
+        }
+    });
+    
+    
+    
+    
+    
+    
+    
+    
 });
-
-
