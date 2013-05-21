6762bca92bbe2c480813c8e8525c47a168d675e7
Nafie Gorman <nafienafie@gmail.com>
2013-05-21 13:25:55 -0700

    Created scripts.js and added waypoints for nav

---
+++ Velo Gioielli/scripts/scripts.js
@@ -0,0 +1,56 @@
+$(function() {
+		// jQuery code goes here
+
+
+
+
+  // LINK TO SECTION OF PAGE
+    $('.linkzero').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".aboutSection", 800);
+        
+    });
+    // LINK TO SECTION OF PAGE
+    $('.linkone').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".sectionHeaderSub", 800);
+        
+    });
+        
+    // LINK TO SECTION OF PAGE
+    $('.linktwo').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".sectionHeaderSub", 800);
+    });
+        
+        // LINK TO SECTION OF PAGE
+    $('.linkthree').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".wholesaleSubhead", 800);
+    });
+    // LINK TO SECTION OF PAGE
+    $('.linkfour').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".", 800);
+    });
+    
+    // LINK TO SECTION OF PAGE
+    $('.linkfive').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".", 800);
+        
+    });
+     // LINK TO SECTION OF PAGE
+    $('.linksix').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".", 800);
+        
+    });
+
+
+
+
+	
+	
+	
+});
