9a58477709237a1f01f19a126e3350c84bad38e8
Chance Tagg <chancetagg@gmail.com>
2013-05-21 19:18:02 -0700

    Merge branch 'master' of https://github.com/chancetagg/team_velo
    Conflicts:
    Velo Gioielli/index.html
    Velo Gioielli/scripts/scripts.js
    Velo Gioielli/styles/css/styles.css
    Velo Gioielli/styles/scss/styles.scss

--- Velo Gioielli/scripts/scripts.js
+++ Velo Gioielli/scripts/scripts.js
@@ -1,55 +1,73 @@
+<<<<<<< HEAD
 
 
 
-function slideSwitch() {
-    var $active = $('#slideshow .slide.active');
-
-    if ( $active.length == 0 ) $active = $('#slideshow .slide:last');
-
-    // use this to pull the divs in the order they appear in the markup
-    var $next =  $active.next().length ? $active.next()
-        : $('#slideshow .slide:first');
-
-    // uncomment below to pull the divs randomly
-    // var $sibs  = $active.siblings();
-    // var rndNum = Math.floor(Math.random() * $sibs.length );
-    // var $next  = $( $sibs[ rndNum ] );
+=======
 
 
-    $active.addClass('last-active');
 
-    $next.css({opacity: 0.0})
-        .addClass('active')
-        .animate({opacity: 1.0}, 1000, function() {
-            $active.removeClass('active last-active');
-        });
-}
+
+
+
+
+
+
+
 
 $(function() {
-    setInterval( "slideSwitch()", 5000 );
-});
+		// jQuery code goes here
 
 
-$(document).ready(function() {
-//alert('js working')
-	
-	//Calculate the height of <header>
-	//Use outerHeight() instead of height() if have padding
-    var aboveHeight = $('header').outerHeight();
-	
-	// when scroll
-    $(window).scroll(function(){
-		
-		//if scrolled down more than the header's height
-        if ($(window).scrollTop() > aboveHeight){
-			
-			// if yes, add "fixed" class to the <nav>
-			// add padding top to the #content (value is same as the height of the nav)
-            $('.navBar').addClass('fixed').css('top','0').next().css('padding-top','90px');
-        } else {
-			
-			// when scroll up or less than aboveHeight, remove the "fixed" class, and the padding-top
-            $('.navBar').removeClass('fixed').next().css('padding-top','0');
-        }
+
+
+
+  // LINK TO SECTION OF PAGE
+    $('.linkzero').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".aboutSection", 800);
+        
     });
+    // LINK TO SECTION OF PAGE
+    $('.linkone').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".sectionHeaderSub", 800);
+        
+    });
+        
+    // LINK TO SECTION OF PAGE
+    $('.linktwo').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".sectionHeaderSub", 800);
+    });
+        
+        // LINK TO SECTION OF PAGE
+    $('.linkthree').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".wholesaleSubhead", 800);
+    });
+    // LINK TO SECTION OF PAGE
+    $('.linkfour').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".", 800);
+    });
+    
+    // LINK TO SECTION OF PAGE
+    $('.linkfive').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".", 800);
+        
+    });
+     // LINK TO SECTION OF PAGE
+    $('.linksix').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".", 800);
+        
+    });
+
+
+
+	
+	
+	
+>>>>>>> 02370376ea78571d9cb9725b93f0887a2fbc8123
 });
