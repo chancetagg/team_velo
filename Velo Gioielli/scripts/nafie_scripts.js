9a4324a7dc7dab4fc83fac82d40c736e904b7cbd
Nafie Gorman <nafienafie@gmail.com>
2013-05-23 10:24:03 -0700

    created nafie_scripts and linked to it in the HEAD

---
+++ Velo Gioielli/scripts/nafie_scripts.js
@@ -0,0 +1,49 @@
+
+$(document).ready(function() {
+		// jQuery code goes here
+		$(window).stellar();
+
+
+
+
+
+  // LINK TO SECTION OF PAGE
+    $('.linkzero').click(function() {
+
+        alert('js working');
+        // TARGET --> SPEED
+        $(window).scrollTo(".aboutSection", 800);
+    });
+    
+    // LINK TO SECTION OF PAGE
+    $('.linkone').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".sectionHeaderSub", 800);
+        
+    });
+        
+    // LINK TO SECTION OF PAGE
+    $('.linktwo').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".sectionHeaderSub", 800);
+    });
+        
+        // LINK TO SECTION OF PAGE
+    $('.linkthree').click(function() {
+        // TARGET --> SPEED
+        $(window).scrollTo(".wholesaleSubhead", 800);
+    });
+
+  
+  /**/
+  
+  
+  
+  
+  
+  
+  
+
+
+});
+
