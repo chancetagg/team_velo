3209f3ab3570c9cc1fa585b4a10160755dd77256
Nafie Gorman <nafienafie@gmail.com>
2013-05-23 09:35:39 -0700

    stellar changes

--- Velo Gioielli 5.22/scripts/scripts.js
+++ Velo Gioielli 5.22/scripts/scripts.js
@@ -1,29 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-
-
-=======
-
-
-
-
-
-
-
-
-
-
-
-$(function() {
-		// jQuery code goes here
-
-
-=======
-$(function() {
+$(document).ready(function() {
 		// jQuery code goes here
 		$(window).stellar();
->>>>>>> c679abd5cbacb6aec3210ccbdfc74ab4946ff333
+
 
 
 
@@ -51,38 +30,9 @@ $(function() {
         // TARGET --> SPEED
         $(window).scrollTo(".wholesaleSubhead", 800);
     });
-<<<<<<< HEAD
-    // LINK TO SECTION OF PAGE
-    $('.linkfour').click(function() {
-        // TARGET --> SPEED
-        $(window).scrollTo(".", 800);
-    });
-    
-    // LINK TO SECTION OF PAGE
-    $('.linkfive').click(function() {
-        // TARGET --> SPEED
-        $(window).scrollTo(".", 800);
-        
-    });
-     // LINK TO SECTION OF PAGE
-    $('.linksix').click(function() {
-        // TARGET --> SPEED
-        $(window).scrollTo(".", 800);
-        
-    });
-=======
+
   
 
->>>>>>> c679abd5cbacb6aec3210ccbdfc74ab4946ff333
 
-
-
-	
-	
-	
-<<<<<<< HEAD
->>>>>>> 02370376ea78571d9cb9725b93f0887a2fbc8123
 });
-=======
-});
->>>>>>> c679abd5cbacb6aec3210ccbdfc74ab4946ff333
+
