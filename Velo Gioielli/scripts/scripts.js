7418197c715da80ef5671de3a52ba1035adc2f63
Nafie Gorman <nafienafie@gmail.com>
2013-06-03 15:11:45 -0700

    turned on Adapt which sucked so I turned it off.

--- Velo Gioielli/scripts/scripts.js
+++ Velo Gioielli/scripts/scripts.js
@@ -1,4 +1,22 @@
-	
+/*
+
+
+var ADAPT_CONFIG = {
+  path: 'assets/css/',
+  dynamic: true,
+
+  range: [
+    '0px    to 760px  = mobile.css',
+    '760px  to 980px  = 720.css',
+    '980px  to 1280px = 960.css',
+    '1280px to 1600px = 1200.css',
+    '1600px to 1920px = 1560.css',
+    '1920px           = fluid.css'
+  ]
+};
+*/
+
+
 
 
 $(document).ready(function() {
